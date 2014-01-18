c5b8f779122a60b5a6f203f8e1654d5ab74ac8da
yuankunluo <yuankun1988@gmail.com>
2014-01-18 07:20:41 +0100

    html design over

--- main.py
+++ main.py
@@ -4,14 +4,20 @@ Created on Tue Jan 14 17:38:53 2014
 
 @author: yuankunluo
 """
+import bottle
+from bottle import route, run, Bottle, template
+from bottle import SimpleTemplate as Tpl
+import os
 
-from bottle import route, run, Bottle
+path = os.path.dirname(os.path.abspath(__file__))+"/views/"
+# change template folder
+bottle.TEMPLATE_PATH.insert(0,'path')
 
 app = Bottle(autojson=True)
 
 @app.route("/") # homepage
 def homepage():
-    return "Welcome"
+    return "hello world"
 
 run(app,host="127.0.0.1",port=8080)
 
---
+++ test.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Jan 18 02:06:44 2014
+
+@author: yuankunluo
+"""
+import os
+
+a = os.path.dirname(os.path.abspath(__file__))
